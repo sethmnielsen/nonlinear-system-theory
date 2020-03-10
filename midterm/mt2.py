d2471db7607c7c04762ce7d1e22509cc261fe51d
Seth Nielsen <sethmnielsen@gmail.com>
2020-03-10 04:15:36 -0600

    Auto-pushed changes

--- midterm/mt2.py
+++ midterm/mt2.py
@@ -4,18 +4,30 @@ import sys
 sys.path.append("..")
 from phase_portrait import PhasePlotter
 
-def calc_xdot(x, t0=0, mu=-1.4):
+def calc_xdot(x, t0=0):
     x1 = x[0]
     x2 = x[1]
+    x3 = x[2]
    
+    rho = 2
+    b = 1.5
+    c1 = 1
+    c2 = 2
+    c3 = 3
+    Ef = 7
+    I = 12
+    tau = 0.6
+    
     x1d = x2 
-    x2d = -x1 + x2*mu - x2*x1**2 - x2**3
-    return np.array([x1d, x2d])
+    x2d = (1/I) * ( rho -b*x2 - c1*x3*np.sin(x1) )
+    x3d = (1/tau) * ( -c2*x3 + c3*np.cos(x1) + Ef )
+    return [x1d, x2d, x3d] 
     
 props = {
+    'ndim': 3,
     'width': 4.0,
     'mgrid_sz': 40,
-    'figsize': [18,18],
+    'figsize': [10,10],
     'style': None,
     'rc': {'axes.facecolor': '#252538'}
 }
--- midterm/phase_portrait.py
+++ midterm/phase_portrait.py
@@ -6,6 +6,7 @@ from scipy import integrate
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.figure import Figure
 import seaborn as sns
 
@@ -15,9 +16,17 @@ class PhasePlotter:
     
     def __init__(self, eqpts, calc_xdot: callable, props:Dict=None):
         self.set_properties(props)
+        is_3d = False 
+        proj = None
+        if self.NDIM == 3:
+            is_3d = True
+            proj = '3d'
+        self.fig1, self.ax1 = plt.subplots(subplot_kw=dict(projection=proj))
         
-        self.fig1, self.ax1 = plt.subplots(figsize=(18,18))
-        self.ax1: Axes
+        if is_3d:
+            self.ax1: Axes3D
+        else:
+            self.ax1: Axes
         
         self.eqpts = np.array(eqpts)
         self.calc_xdot = calc_xdot
@@ -26,31 +35,41 @@ class PhasePlotter:
         w = self.PLOT_WIDTH 
         a = self.PLOT_OFFSET 
         m = self.MGRID_SIZE 
-        x_arr = np.vstack(( np.linspace(-abs(w+a), w+a, m), np.linspace(-abs(w+a), w+a, m) ))
-        x1, x2 = np.meshgrid(x_arr[0], x_arr[1])
+        x_arr = np.vstack([np.linspace(-abs(w+a), w+a, m) for i in range(self.NDIM)])
+        self.xmesh = np.meshgrid(*x_arr)
         
-        self.x1d, self.x2d = self.calc_xdot([x1,x2])
-       
+        self.xdot = self.calc_xdot(self.xmesh)
         
-        self.x1, self.x2 = x1, x2
+        self.x1, self.x2 = self.xmesh
+        self.x1d, self.x2d = self.xdot
         
     def plot_axes(self):
         w = self.PLOT_WIDTH
         
-        self.ax1.plot([-w,w], [0,0], color='0', linewidth='0.7')
-        self.ax1.plot([0,0], [-w,w], color='0', linewidth='0.7')
-        self.ax1.set_aspect('equal')
         self.ax1.set_xlim(-w,w)
         self.ax1.set_ylim(-w,w)
+        if self.NDIM == 3: 
+            self.ax1.set_zlim(-w,w)
+        else:
+            self.ax1.plot([-w,w], [0,0], color='0', linewidth='0.7')
+            self.ax1.plot([0,0], [-w,w], color='0', linewidth='0.7')
+        self.ax1.set_aspect('equal')
         
     
     def plot_phases(self, params:Dict=None):
         self.states_meshgrid()
         self.plot_axes()        
 
-        x1d_ulen = self.x1d / np.sqrt(self.x1d**2 + self.x2d**2) 
-        x2d_ulen = self.x2d / np.sqrt(self.x1d**2 + self.x2d**2)
-        M = np.hypot(self.x1d, self.x2d)
+        # x1d_ulen = self.x1d / np.sqrt(self.x1d**2 + self.x2d**2) 
+        # x2d_ulen = self.x2d / np.sqrt(self.x1d**2 + self.x2d**2)
+        xdot_ulen = np.zeros(self.xdot.shape) 
+        norm2 = np.sqrt(np.sum(self.xdot**2, axis=0))
+        for i, xd in enumerate(self.xdot):
+            xdot_ulen[i] = xd / norm2 
+            
+        # x1d_ulen = self.x1d / np.sqrt(self.x1d**2 + self.x2d**2) 
+        # x2d_ulen = self.x2d / np.sqrt(self.x1d**2 + self.x2d**2)
+        # M = np.hypot(self.x1d, self.x2d)
         
         params_ = {'units': 'xy',
                    'angles': 'xy',
@@ -68,7 +87,8 @@ class PhasePlotter:
                 for key in matplotlib.cm.cmap_d.keys():
                     cmap = key if cmap.casefold() == key.casefold() else cmap
             params_['cmap'] = cmap
-        Q = self.ax1.quiver(self.x1, self.x2, x1d_ulen, x2d_ulen, M, **params_)
+        # Q = self.ax1.quiver(self.x1, self.x2, self.xd1, norm2, **params_)
+        Q = self.ax1.quiver(*self.xmesh, *xdot_ulen, norm2, **params_)
         
 
     def plot_trajectories(self, num:int=10, params=None):
@@ -115,13 +135,16 @@ class PhasePlotter:
         self.ax1.clabel(CS, inline=1, fontsize=10)
     
     def set_properties(self, props:Dict=None):
-        props_ = {'figsize': [10,10],
-                  'style': None, 
-                  'width': 4,
-                  'offset': 0,
-                  'mgrid_sz': 42,
-                  'seed': None,
-                  'rc': None}
+        props_ = {
+            'ndim': 2,
+            'figsize': [10,10],
+            'style': None, 
+            'width': 4,
+            'offset': 0,
+            'mgrid_sz': 42,
+            'seed': None,
+            'rc': None
+        }
         
         if props is not None:
             for key in props.keys():
@@ -136,6 +159,8 @@ class PhasePlotter:
         self.MGRID_SIZE = props_['mgrid_sz']
         
         self.RNG_SEED = props_['seed']
+        
+        self.NDIM = props_['ndim']
         
     def draw_plots(self):
         plt.show()
